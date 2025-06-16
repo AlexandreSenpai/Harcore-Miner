2412f85a0274ec8cbf8b36f5413a169e9a0de09a
Guilherme Simonato <guilhermesimonato4@gmail.com>
2025-06-15 22:17:27 -0300

    HUD É TOP

---
+++ main/scripts/game_manager.lua
@@ -0,0 +1,13 @@
+local status = {
+    level = 1,
+    gold = 100000,
+    damage = 1,
+    timer = 15
+}
+
+local shopping = {
+    damage = 10,
+    timer = 10
+}
+
+return {status = status, shopping = shopping}
